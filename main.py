fd85816eeb90bef6726713ea0da2acc8f27d8502
raackley <raackley@protonmail.com>
2022-10-02 20:07:16 +0000

    Cleanup code and Readme

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 import Adafruit_BMP.BMP085 as BMP085
+
+
 # initialize our Flask application
-app= Flask(__name__)
-@app.route("/temp", methods=["GET"])
-def temp():
-    sensor = BMP085.BMP085()
-    return jsonify('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+app = Flask(__name__)
+
+
 @app.route("/metrics", methods=["GET"])
 def metrics():
     sensor = BMP085.BMP085()
     return 'sensor_temp_c {0:0.2f}\n'.format(sensor.read_temperature())
+
+
 #  main thread of execution to start the server
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=False)
